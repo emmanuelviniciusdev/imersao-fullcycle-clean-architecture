f6a78a6b27cb04a7d2b7db151fe914ff5c5339f6
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-08-06 09:12:13 -0300

    [CORREÇÃO] DESAFIO: "VALIDANDO PRODUCT"

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -12,6 +12,30 @@ describe('Product E2E Tests', () => {
         await sequelize.close()
     })
 
+    it('should create a product', async () => {
+        const body: any = {
+            name: 'Pumpkin',
+            price: 0.8,
+        }
+
+        const response = await request(app).post('/product').send(body)
+
+        expect(response.status).toBe(201)
+        expect(response.body.name).toBe('Pumpkin')
+        expect(response.body.price).toBe(0.8)
+    })
+
+    it('should not create a product', async () => {
+        const body: any = {
+            name: null,
+            price: null,
+        }
+
+        const response = await request(app).post('/product').send(body)
+
+        expect(response.status).toBe(500)
+    })
+
     it('should list all products', async () => {
         await request(app).post('/product').send({
             name: 'Pumpkin',
